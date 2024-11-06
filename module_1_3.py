20914e6d87c3c06f4b72e0ea8d57678aa044115a
Dima <72711928+dmytros977@users.noreply.github.com>
2024-11-06 15:15:33 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = "Dmytro"
+print(name, type(name))
+age = 23
+print(age, type(23))
+new_age = [24]
+print(new_age, type(new_age))
+is_student = True
+print(is_student)
+print(is_student, type(is_student))
